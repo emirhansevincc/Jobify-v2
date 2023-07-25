e5ae19bea4e9118a99c530d7d9943ff2cc575a4c
Emirhan Sevinc <emirhansvncc@gmail.com>
2023-07-25 17:53:08 +0300

    Landing Page

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -2,12 +2,9 @@ import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
   return (
-    <div>
-      {/* add things like Navbar */}
-      {/* <h1>home layout</h1> */}
-      <h1>Home Layout</h1>
+    <>
       <Outlet />
-    </div>
+    </>
   );
 };
 export default HomeLayout;
