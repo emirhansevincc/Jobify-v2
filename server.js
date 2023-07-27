c0ba35d0a7f77d67ca1e9d63f3603af6a0389eab
Emirhan Sevinc <emirhansvncc@gmail.com>
2023-07-27 23:24:45 +0300

    Local CRUD Functionality

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import morgan from "morgan";
+import { nanoid } from "nanoid";
 
 const app = express();
-
 app.use(express.json());
+
+let jobs = [
+    { id: nanoid(), company: "apple", position: "front-end" },
+    { id: nanoid(), company: "google", position: "back-end" },
+];
 
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
@@ -18,6 +23,64 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
     res.send('POST request to the / endpoint');
 })
+
+// Get all jobs
+app.get("/api/v1/jobs", (req, res) => {
+    res.status(200).json({ jobs });
+});
+
+// Create a job
+app.post("/api/v1/jobs", (req, res) => {
+    const { company, position } = req.body;
+    if (!company || !position) {
+        return res.status(400).json({ msg: "please provide company and position" });
+    }
+    const id = nanoid(10);
+    // console.log(id);
+    const job = { id, company, position };
+    jobs.push(job);
+    res.status(201).json({ job });
+});
+
+// GET SINGLE JOB
+app.get("/api/v1/jobs/:id", (req, res) => {
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` });
+    }
+    res.status(200).json({ job });
+});
+
+// Edit a job
+app.patch("/api/v1/jobs/:id", (req, res) => {
+    const { company, position } = req.body;
+    if (!company || !position) {
+        return res.status(400).json({ msg: "please provide company and position" });
+    }
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` });
+    }
+
+    job.company = company;
+    job.position = position;
+    res.status(200).json({ msg: "job modified", job });
+});
+
+// DELETE JOB
+app.delete("/api/v1/jobs/:id", (req, res) => {
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` });
+    }
+    const newJobs = jobs.filter((job) => job.id !== id);
+    jobs = newJobs;
+
+    res.status(200).json({ msg: "job deleted" });
+});
 
 const port = process.env.PORT || 5100;
 app.listen(port, () => {
