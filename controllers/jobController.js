bdb916bcab68c4c0a4ec778d6a2750cd372f3402
Emirhan Sevinc <emirhansvncc@gmail.com>
2023-07-28 16:55:34 +0300

    Job Model and Create Job Controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,4 +1,5 @@
 import { nanoid } from "nanoid";
+import Job from "../models/JobModel.js";
 
 let jobs = [
     { id: nanoid(), company: "apple", position: "front-end" },
@@ -10,14 +11,7 @@ export const getAllJobs = async (req, res) => {
 };
 
 export const createJob = async (req, res) => {
-    const { company, position } = req.body;
-    if (!company || !position) {
-        return res.status(400).json({ msg: "please provide company and position" });
-    }
-    const id = nanoid(10);
-    // console.log(id);
-    const job = { id, company, position };
-    jobs.push(job);
+    const job = await Job.create(req.body);
     res.status(201).json({ job });
 };
 
