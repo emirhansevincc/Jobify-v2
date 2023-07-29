bd533437b09757dadee0985db1f41ce4b58b5f2d
Emirhan Sevinc <emirhansvncc@gmail.com>
2023-07-29 23:32:15 +0300

    Validate Job | our custom errors included and minor changes in withValidationErrors function

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,7 +1,8 @@
 import { body, param, validationResult } from "express-validator";
-import { BadRequestError } from "../errors/customErrors.js";
+import { BadRequestError, NotFoundError } from "../errors/customErrors.js";
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
 import mongoose from "mongoose";
+import Job from "../models/JobModel.js";
 
 const withValidationErrors = (validateValues) => { // validateValues is an array of validation functions
     return [
@@ -10,6 +11,9 @@ const withValidationErrors = (validateValues) => { // validateValues is an array
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
                 const errorMessages = errors.array().map((error) => error.msg);
+                if (errorMessages[0].startsWith("no job")) {
+                    throw new NotFoundError(errorMessages);
+                }
                 throw new BadRequestError(errorMessages);
             }
             next();
@@ -30,6 +34,13 @@ export const validateJobInput = withValidationErrors([
 export const validateIdParam = withValidationErrors([
     // we use 'id' because that's what we named the parameter in the route
     param("id")
-        .custom((value) => mongoose.Types.ObjectId.isValid(value)) // It returns true if the value is a valid MongoDB ObjectId else false.
-        .withMessage("invalid MongoDB id"),
-]);
+        .custom(async (value) => {
+            const isValidId = mongoose.Types.ObjectId.isValid(value)
+            // We dont need withMessage() because we are throwing our error
+            if (!isValidId) throw new BadRequestError("invalid MongoDB id");
+            const job = await Job.findById(value);
+            if (!job) throw new NotFoundError(`no job with id ${value}`);
+
+        }) // It returns true if the value is a valid MongoDB ObjectId else false.
+    // .withMessage("invalid MongoDB id"),
+]); 
