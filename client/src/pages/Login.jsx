e525f8afffe1003f768b38843ea8e090d4075360
Emirhan Sevinc <emirhansvncc@gmail.com>
2023-08-02 10:13:09 +0300

    Add Job Structure

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
         <Logo />
         <h4>Login</h4>
         <FormRow type="email" name="email" defaultValue="john@gmail.com" />
-        <FormRow type="password" name="password" defaultValue="secret123" />
+        <FormRow type="password" name="password" defaultValue="john-123" />
         <button type="submit" className="btn btn-block" disabled={isSubmitting}>
           {isSubmitting ? "submitting..." : "submit"}
         </button>
