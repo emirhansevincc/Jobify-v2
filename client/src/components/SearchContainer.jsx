f3f505bef2ce4226c6d9841409cd545e52d2f03b
Emirhan Sevinc <emirhansvncc@gmail.com>
2023-08-04 22:06:13 +0300

    Search Form Setup

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { FormRow, FormRowSelect, SubmitBtn } from ".";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { Form, useSubmit, Link } from "react-router-dom";
+import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from "../../../utils/constants";
+import { useAllJobsContext } from "../pages/AllJobs";
 
 const SearchContainer = () => {
-  return <div>SearchContainer</div>;
+  return (
+    <Wrapper>
+      {/* We didn't use method="post" like previous forms because we don't want to send data to the server if you dont use a method name, it going to be a get request by default and add query params to the url */}
+      <Form className="form">
+        <h5 className="form-title">search form</h5>
+        <div className="form-center">
+          {/* search position */}
+
+          <FormRow type="search" name="search" defaultValue="a" />
+          <FormRowSelect
+            labelText="job status"
+            name="jobStatus"
+            list={["all", ...Object.values(JOB_STATUS)]}
+            defaultValue="all"
+          />
+          <FormRowSelect
+            labelText="job type"
+            name="jobType"
+            list={["all", ...Object.values(JOB_TYPE)]}
+            defaultValue="all"
+          />
+          <FormRowSelect
+            name="sort"
+            defaultValue="newest"
+            list={[...Object.values(JOB_SORT_BY)]}
+          />
+
+          <Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
+            Reset Search Values
+          </Link>
+          {/* TEMP!!!! */}
+          <SubmitBtn formBtn />
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default SearchContainer;
