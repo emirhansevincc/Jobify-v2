21a3727822db623eb8ccf28a3750e6e8cf59e591
Emirhan Sevinc <emirhansvncc@gmail.com>
2023-08-04 13:23:48 +0300

    Stats Route Setup

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 const router = Router();
 
-import { getAllJobs, createJob, getJob, updateJob, deleteJob } from "../controllers/jobController.js";
+import { getAllJobs, createJob, getJob, updateJob, deleteJob, showStats } from "../controllers/jobController.js";
 import { validateJobInput, validateIdParam } from "../middleware/validationMiddleware.js";
 import { checkForTestUser } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getAllJobs).post(checkForTestUser, validateJobInput, createJob);
+
+router.route("/stats").get(showStats);
+
 router
     .route("/:id")
     .get(validateIdParam, getJob)
